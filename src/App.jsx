8759c21394def23df71f52d0e4b684e180f8887b
“johnrussell <jonnycryptokings@gmail.com>
2023-08-14 21:03:37 -0400

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
 // Write your Color component here
+import { useState } from 'react';
 
+const Color = () => {
+  return <div className="red"></div>
+  
+}
 const App = () => {
   return (
-    <div id="container">
-      <div id="navbar">
-        <div>Currently selected: </div>
-        <div className="red">red</div>
-      </div>
-      <div id="colors-list">{/* colors go here */}</div>
-    </div>
+    <><div id="navbar">
+      <div>Currently selected: </div>
+      <div className={selectedColor}>{selectedColor}</div>
+    </div><div id="container">
+        <div id="navbar">
+          <div>Currently selected: </div>
+          <div className="red">red</div>
+        </div>
+        <div id="colors-list">{<div id="colors-list">
+          <div id="colors-list">
+            <Color color="red" />
+            <Color color="blue" />
+            <Color color="green" />
+          </div>
+          <div id="colors-list">
+  <Color color="red" setSelectedColor={setSelectedColor}/>
+  <Color color="blue" setSelectedColor={setSelectedColor}/>
+  <Color color="green" setSelectedColor={setSelectedColor}/>
+</div>
+
+        </div>}</div>
+      </div></>
   );
 };
 
